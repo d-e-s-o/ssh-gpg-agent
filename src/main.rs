13a40f22b578c7caa1da971bbd8b9ef697e3d50d
Daniel Mueller <deso@posteo.net>
2019-02-09 15:07:30 -0800

    Add agent scaffolding
    This change introduces the basic scaffolding for the SSH agent. We rely
    on the ssh-agent crate for dispatching agent protocol messages to us.
    The agent will discover keys in the default SSH configuration directory:
    ~/.ssh. It will listen on a Unix domain socket in the default temporary
    directory.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,67 @@
 // * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 // *************************************************************************
 
-fn main() {
-  println!("Hello, world!");
+//! `ssh-gpg-agent` is an SSH agent that can transparently handle GPG
+//! encrypted SSH keys.
+
+use std::env::temp_dir;
+use std::error::Error as StdError;
+use std::fs::remove_file;
+use std::io::Error as IoError;
+use std::io::ErrorKind;
+use std::path::PathBuf;
+use std::result::Result;
+
+use dirs::home_dir;
+
+use log::error;
+
+use ssh_agent::agent::Agent;
+use ssh_agent::proto::message::Message;
+
+
+/// The SSH agent supporting GPG encrypted SSH keys.
+struct GpgKeyAgent {
+}
+
+impl GpgKeyAgent {
+  fn new<P>(dir: P) -> Self
+  where
+    P: Into<PathBuf>,
+  {
+    Self {}
+  }
+
+  /// Handle a message to the agent.
+  fn handle_message(&self, request: Message) -> Result<Message, ()> {
+    unimplemented!()
+  }
+}
+
+impl Agent for GpgKeyAgent {
+  type Error = ();
+
+  fn handle(&self, message: Message) -> Result<Message, ()> {
+    self.handle_message(message).or_else(|err| {
+      error!("Error handling message: {:?}", err);
+      Ok(Message::Failure)
+    })
+  }
+}
+
+
+/// Run the SSH agent.
+fn main() -> Result<(), Box<StdError>> {
+  env_logger::init();
+
+  let ssh_dir = home_dir()
+    .ok_or_else(|| IoError::new(ErrorKind::NotFound, "no home directory found"))?
+    .join(".ssh");
+
+  let agent = GpgKeyAgent::new(ssh_dir);
+  let socket = temp_dir().join("ssh-gpg-agent.sock");
+  let _ = remove_file(&socket);
+
+  agent.run_unix(&socket)?;
+  Ok(())
 }
